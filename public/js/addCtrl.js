c416db4c2d4175a9864bad68c4575cd992566017
Daniel Chen <dchen880@gmail.com>
2016-07-23 21:54:46 -0700

    Added file directories

---
+++ public/js/addCtrl.js
@@ -0,0 +1,69 @@
+
+// Creates the addCtrl Module and Controller. Note that it depends on the 'geolocation' module and service.
+var addCtrl = angular.module('addCtrl', ['geolocation', 'gservice']);
+addCtrl.controller('addCtrl', function($scope, $http, $rootScope, geolocation, gservice){
+
+    // Initializes Variables
+    // ----------------------------------------------------------------------------
+    $scope.formData = {};
+    var coords = {};
+    var lat = 0;
+    var long = 0;
+
+    // Set initial coordinates to the center of the US
+    $scope.formData.latitude = 39.500;
+    $scope.formData.longitude = -98.350;
+
+    // Functions
+    // ----------------------------------------------------------------------------
+    // Creates a new user based on the form fields
+    $rootScope.$on("clicked", function(){
+        // Get coordinates based on mouse click. When a click event is detected....
+
+        // Run the gservice functions associated with identifying coordinates
+        $scope.$apply(function(){
+            $scope.formData.latitude = parseFloat(gservice.clickLat).toFixed(3);
+            $scope.formData.longitude = parseFloat(gservice.clickLong).toFixed(3);
+            $scope.formData.htmlverified = "Nope (Thanks for spamming my map...)";
+        });
+    });
+
+// Create User Function
+// ...
+
+
+    $scope.createUser = function() {
+
+        // Grabs all of the text box fields
+        var userData = {
+            username: $scope.formData.username,
+            gender: $scope.formData.gender,
+            age: $scope.formData.age,
+            favlang: $scope.formData.favlang,
+            location: [$scope.formData.longitude, $scope.formData.latitude],
+            htmlverified: $scope.formData.htmlverified
+        };
+
+        // Saves the user data to the db
+        $http.post('/users', userData)
+            .success(function (data) {
+
+                // Once complete, clear the form (except location)
+                $scope.formData.username = "";
+                $scope.formData.gender = "";
+                $scope.formData.age = "";
+                $scope.formData.favlang = "";
+                // Logic for Clearing the FOrm
+                // ...
+
+                // Refresh the map with new data
+                gservice.refresh($scope.formData.latitude, $scope.formData.longitude);
+
+            })
+
+            .error(function (data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+});
